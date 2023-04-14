885495c0ecf33a9c957c73cd0745dd86b5a6da3f
Joseph Shumaker <59636597+s5y-ux@users.noreply.github.com>
2023-04-13 18:12:37 -0700

    Update Join+.js

--- Join+.js
+++ Join+.js
@@ -1,8 +1,13 @@
+//Referencing the JSON file for user configuration
 var test = require('./scriptcraft/plugins/FirstJoin+/config.json');
+
+//Used to reference player position
 var utils = require('utils');
+
+//Variable used to store package for particle effects
 var particle_handler = Packages.org.bukkit.Particle;
 
-
+//Dictionary for referencing particles using strings
 var particle_list = {
     "cloud": particle_handler.CLOUD,
     "crit": particle_handler.CRIT,
@@ -15,6 +20,7 @@ var particle_list = {
     "heart": particle_handler.HEART
 };
 
+//Switch statement for applying color according to JSON file
 function apply_color(object, color) {
     switch (color) {
         case "red":
@@ -59,10 +65,19 @@ function apply_color(object, color) {
     }
 }
 
+//Listens for player joining
 events.playerJoin(function(event, player) {
+    
+    //used to reference the player
     var player_name = event.player.getName();
+    
+    //used to reference the player position
     Player_Position = utils.getPlayerPos(utils.player(player_name));
+    
+    //Sends the title to the player with the title and color according to the JSON file
     event.player.sendTitle(apply_color(test.title, test.title_color), apply_color(test.sub_title, test.sub_title_color));
+    
+    //Spawns the particle 10 times with the particle effect up to the JSON file
     for (i = 0; i < 10; i++) {
         event.player.spawnParticle(particle_list[test.particle], Player_Position.x, Player_Position.y + 0.5, Player_Position.z, 15);
     }
